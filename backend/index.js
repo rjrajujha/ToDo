8b2355dcf31c159d258947f0271db4d078d12edf
rjrajujha <rjrajujha@outlook.com>
2023-01-07 11:46:09 +0530

    will complete on replit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+// const db = mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// db.then(() => {
+//     console.log("Connected to DataBase")
+// }).catch((e) => {
+//     console.log(`Error ${e}`)
+// });
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up at port ${port}`);
+})
