ea9352c914216bcb9e090382487d181587547f97
rjrajujha <rjrajujha@outlook.com>
2023-02-07 17:29:51 +0530

    put and delete updated

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 //User schema
 const ToDos = require("../models/todos");
+
+// parse application/x-www-form-urlencoded
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+router.use(bodyParser.json())
 
 // define the all ToDos route
 router.get('/', async (req, res) => {
@@ -23,11 +30,56 @@ router.get('/', async (req, res) => {
 })
 
 // Add a ToDo
-router.post('/add', (req, res) => {
-    console.log(req.body);
-    console.log("test");
-    res.end();
+router.post('/add', async (req, res) => {
+    try {
+        const data = await ToDos.create({ "title": req.body.title, "note": req.body.note })
+        res.json({
+            status: "sucess",
+            data
+        });
+        res.end();
+    }
+    catch (error) {
+        res.json({
+            status: "failed",
+            error
+        });
+    }
 })
 
+router.put('/edit', async (req, res) => {
+    try {
+        const data = await ToDos.updateOne({ "_id": req.body._id }, { "title": req.body.title, "note": req.body.note })
+        res.json({
+            status: "sucess",
+            data
+        });
+        res.end();
+    }
+    catch (error) {
+        res.json({
+            status: "failed",
+            error
+        });
+    }
+})
+
+
+router.delete('/del', async (req, res) => {
+    try {
+        const data = await ToDos.deleteOne({ "_id": req.body._id })
+        res.json({
+            status: "sucess",
+            data
+        });
+        res.end();
+    }
+    catch (error) {
+        res.json({
+            status: "failed",
+            error
+        });
+    }
+})
 
 module.exports = router;
