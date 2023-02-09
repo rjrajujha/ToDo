90a43268b12d9a2312b571f4b197f9e1ecdda9ea
rjrajujha <rjrajujha@outlook.com>
2023-02-09 22:33:43 +0530

    init backend connection onLoad

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
 
-
-app.get('/', (req, res) => {
-  res.writeHead(301, { Location: "https://github.com/rjrajujha" });
-  res.end();
-})
-
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
+  // console.log("requestd url :", req.originalUrl);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -16,6 +11,19 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.get('/', (req, res) => {
+  res.writeHead(301, { Location: "https://github.com/rjrajujha" });
+  res.end();
+})
+
+app.post('/init', (req, res) => {
+  res.json({
+    status: "Connected",
+    message: "Backend Connected"
+  })
+  res.end();
+})
 
 app.use('/login', require("./routes/login"));
 app.use('/signup', require("./routes/signup"));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,31 @@ import LogIn from './pages/login';
 import Register from './pages/register'
 import ToDos from './pages/todoList';
 import NoPage from './pages/nopage';
+import axios from "axios";
+
+const APIUrl = process.env.REACT_APP_APIURL;
 
 function App() {
+
+  //Environment Variables Configured Message
   console.log(process.env.REACT_APP_ENV_STATUS);
+
+  //Initializing backend connection
+  axios
+    .post(`${APIUrl}/init`)
+    .then((res) => {
+      if (res.data.status === "Connected") {
+        console.log(res.data.message);
+      }
+      else {
+        console.log("Error Connecting Database");
+      }
+    }).catch((e) => {
+      console.log("Error Connecting Backend :", e)
+    }).finally(() => {
+      console.log("Backend Connection finished")
+    })
+
   return (
     <div className="App">
       <BrowserRouter>
